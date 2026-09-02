3299edf8a86963c4307317848f4785f8601fe6e5
qaffasyah-maker <qaf.fasyah@gmail.com>
2026-09-02 14:21:53 +0700

    rollback urls.py

--- portofolio/urls.py
+++ portofolio/urls.py
@@ -16,14 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
+
 from portofolio.views import landing_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', landing_page, name='landing_page'),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
