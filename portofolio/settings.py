50154ee93ed40e9c5a22d4cf8fc83025ef738771
qaffasyah-maker <qaf.fasyah@gmail.com>
2026-09-14 23:21:18 +0700

    memperbaiki csrf

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -149,3 +149,7 @@ MAILERS = {
         'BACKEND': 'django.core.mail.backends.console.EmailBackend',
     },
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://qisthan-albani-myportofolio.pws.cs.ui.ac.id",
+]
