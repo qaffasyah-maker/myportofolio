cb1c0ca8d47fb2d30ef4e3abc858299d1a4174a0
qaffasyah-maker <qaf.fasyah@gmail.com>
2026-09-14 23:24:55 +0700

    memperbaiki models.py

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Experience
+from .models import *
 
 # Register your models here.
 admin.site.register(Experience)
+admin.site.register(Education)
+admin.site.register(Organization)
+admin.site.register(Certificate)
